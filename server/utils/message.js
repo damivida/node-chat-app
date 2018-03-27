6071cc034e0965d87e46bef0663c3e160d339b0b
Damir Vidaković <vida.damir@gmail.com>
2018-03-27 20:02:30 +0200

    auto scrolling option

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,3 +1,4 @@
+var moment = require('moment');
 var generateMessage = (from, text) => {
     
     return {
@@ -7,7 +8,7 @@ var generateMessage = (from, text) => {
     
         from,
         text,
-        createdAt: new Date().getTime()
+        createdAt: moment().valueOf()
     };
 };
 
@@ -16,7 +17,7 @@ var generateLocationMessage = (from, latitude, longitude) => {
     return {
         from,
         url: `https://www.google.com/maps?q=${latitude}, ${longitude}`,
-        createdAt: new Date().getTime()
+        createdAt: moment().valueOf()
     };
 };
 
